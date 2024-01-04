4a0b6b00ab46aa3e3d75286a4740492532bd973b
luc4s-git <lucassantos4332@hotmail.com>
2024-01-04 16:42:59 -0300

    context

---
+++ src/context/context.jsx
@@ -0,0 +1,11 @@
+import { createContext, useContext, useState } from 'react';
+
+const GlobalContext = createContext();
+
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const AppContext = ({ children }) => {
+  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+};
+
+export default AppContext;
