61b6be9b48a37c1be47371217372503cb154b038
luc4s-git <lucassantos4332@hotmail.com>
2024-01-05 15:43:58 -0300

    sidebar starter

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,36 @@
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from '../context/context';
+import subLinks from '../data';
+
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  const { closeSidebar } = useGlobalContext();
+
+  const subLinksMapping = subLinks.map((link) => {
+    return (
+      <div key={link.pageId}>
+        <h4>{link.page}</h4>
+        <ul>
+          {link.links.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.url}>
+                  {link.icon}
+                  {link.label}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <button onClick={closeSidebar}>
+        <FaTimes />
+      </button>
+      {subLinksMapping}
+    </div>
+  );
 }
